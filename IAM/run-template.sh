e8194ebba5374e08966307981c29d4655edca42d
Tim Konick <konick781@gmail.com>
2024-01-25 21:41:05 -0500

    Cloudformation Create Dev Role

--- IAM/run-template.sh
+++ IAM/run-template.sh
@@ -1,6 +1,6 @@
 aws cloudformation create-stack \
   --stack-name tijko-role \
-  --template-body file://CreateOpsRole.yaml \
+  --template-body file://CreateDevRole.yaml \
   --profile tijko \
   --capabilities CAPABILITY_IAM \
   --region us-east-2 
